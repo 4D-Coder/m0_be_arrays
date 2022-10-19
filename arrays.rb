daa6ee81b5fc235d2d29568a31add8712eb7d3c1
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2022-10-19 14:20:08 -0600

    Declare variables

---
+++ arrays.rb
@@ -0,0 +1,9 @@
+puts "Vorkath Inventory Setup"
+# Declare a variable that stores an Array of at least 4 Strings
+combat_supplies = ["Manta Rays", "Karambwans", "Dust Runes", "Chaos Rune", "House Teleport Tablets"]
+# Declare a variable that stores an Array of at least 4 Integers
+supplies_amounts = [11, 8, 300, 100, 50]
+# Declare a variable that stores an Array of at least 4 Floats.
+best_times = [2.00, 2.06, 2.18, 2.27]
+# Declare a variable that stores an Array of at least 4 Booleans.
+player_died = [false, false, true, false]
