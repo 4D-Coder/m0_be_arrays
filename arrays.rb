79e45058fec0242ef7504845d0f985cdc05e13d4
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2022-10-19 15:06:25 -0600

    Correct line 42 with proper p command

--- arrays.rb
+++ arrays.rb
@@ -24,7 +24,7 @@ if player_died.pop
     p player_died
 end
 
-# In these variable assignments, I will call the .push() variable on the above Array variables.
+# In these variable assignments, I will call the .push() method on the above Array variables.
 # This will select one Array variable, and add all elements contained within the others 
 # (of course, minus the ones removed with the .pop method) to the end of it's set and print each 
 # combination.
@@ -39,8 +39,13 @@ p sa_first
 
 # best_times elements first
 bt_first = best_times.push(combat_supplies, supplies_amounts, player_died)
-p cs_first
+p bt_first
 
 # player_died elements first
 pd_first = player_died.push(combat_supplies, supplies_amounts, best_times)
 p pd_first
+
+# In these variable assignments, I will call the .shift() method on the above Array variables.
+# These assignments will target index position 0 of each array and remove it from the set,
+# and print the remaining elements of each array.
+
